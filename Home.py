4f2deb990e12877874e8dda854b43962c722de1a
Ramesh De Silva <desilvarami@gmail.com>
2023-05-11 11:52:38 +0530

    To test sync between local machine and GitHub

--- Home.py
+++ Home.py
@@ -3,31 +3,6 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-st.sidebar.title("About")
-st.sidebar.info(
-    """
-    - Web App URL: <https://streamlit.geemap.org>
-    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
-    """
-)
-
-st.sidebar.title("Contact")
-st.sidebar.info(
-    """
-    Qiusheng Wu: <https://wetlands.io>
-    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
-    """
-)
-
-st.sidebar.title("Support")
-st.sidebar.info(
-    """
-    If you want to reward my work, I'd love a cup of coffee from you. Thanks!
-    [buymeacoffee.com/giswqs](http://buymeacoffee.com/giswqs)
-    """
-)
-
-
 st.title("Streamlit for Geospatial Applications")
 
 st.markdown(
