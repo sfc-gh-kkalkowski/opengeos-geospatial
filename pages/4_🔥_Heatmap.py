fc7bd02cb47065b9ef84022d7d6fe0c2ec4941b1
Ramesh De Silva <desilvarami@gmail.com>
2023-05-12 09:09:55 +0530

    Remove Source code block

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -5,16 +5,14 @@ st.set_page_config(layout="wide")
 
 st.title("Heatmap")
 
-with st.expander("See source code"):
-    with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
-        m.add_heatmap(
-            filepath,
-            latitude="latitude",
-            longitude="longitude",
-            value="pop_max",
-            name="Heat map",
-            radius=20,
-        )
+filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
+m.add_heatmap(
+    filepath,
+    latitude="latitude",
+    longitude="longitude",
+    value="pop_max",
+    name="Heat map",
+    radius=20,
+)
 m.to_streamlit(height=700)
