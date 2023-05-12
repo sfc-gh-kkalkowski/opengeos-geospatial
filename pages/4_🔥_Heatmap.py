42dacff5c3a0bb6d49e6df598a279aa121f74dd5
Ramesh De Silva <desilvarami@gmail.com>
2023-05-12 09:12:20 +0530

    Changed to MODIS Hotspots

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -5,14 +5,20 @@ st.set_page_config(layout="wide")
 
 st.title("Heatmap")
 
-filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
+#filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+#m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
+filepath = "./data/modis.csv"
+m = leafmap.Map(ccenter=[0, 0], zoom=2, tiles="stamentoner")
+
 m.add_heatmap(
     filepath,
-    latitude="latitude",
-    longitude="longitude",
-    value="pop_max",
+    #latitude="latitude",
+    #longitude="longitude",
+    #value="pop_max",
+    latitude="LATITUDE",
+    longitude="LONGITUDE",
+    value="CONFIDENCE",
     name="Heat map",
-    radius=20,
+    radius=15,
 )
 m.to_streamlit(height=700)
