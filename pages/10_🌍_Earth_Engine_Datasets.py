e12edcd17534026d1c59f2401ea369d0650a4ad0
Ramesh De Silva <desilvarami@gmail.com>
2023-09-06 11:40:42 +0530

    Update 10_🌍_Earth_Engine_Datasets.py
    remove nlcd():

--- pages/10_🌍_Earth_Engine_Datasets.py
+++ pages/10_🌍_Earth_Engine_Datasets.py
@@ -4,7 +4,6 @@ import geemap.foliumap as geemap
 
 st.set_page_config(layout="wide")
 
-def nlcd():
 def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
     geemap.ee_initialize(token_name=token_name)
 
