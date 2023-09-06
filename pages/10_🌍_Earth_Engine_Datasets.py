71d7ad592aedaa682ee052eb677ddaad975edf45
Ramesh De Silva <desilvarami@gmail.com>
2023-09-06 11:39:11 +0530

    Update 10_🌍_Earth_Engine_Datasets.py
    syntax

--- pages/10_🌍_Earth_Engine_Datasets.py
+++ pages/10_🌍_Earth_Engine_Datasets.py
@@ -5,7 +5,7 @@ import geemap.foliumap as geemap
 st.set_page_config(layout="wide")
 
 def nlcd():
-    ee_authenticate(token_name="EARTHENGINE_TOKEN"):
+def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
     geemap.ee_initialize(token_name=token_name)
 
     # st.header("National Land Cover Database (NLCD)")
