89d6c55c3b332b97151eb71f6e558d391c7d9a15
Ramesh De Silva <desilvarami@gmail.com>
2023-09-11 16:13:20 +0530

    Update 8_⛗_Turkey_EQ_SplitMap.py
    complete URL

--- pages/8_⛗_Turkey_EQ_SplitMap.py
+++ pages/8_⛗_Turkey_EQ_SplitMap.py
@@ -10,9 +10,9 @@ st.title("Turkey Earthquake-Split Map")
 m = leafmap.Map()
 
 
-url_pre = 'https://maxar-opendata.s3.us-west-2.amazonaws.com/events/Kahramanmaras-turkey-earthquake>
+url_pre = 'https://maxar-opendata.s3.us-west-2.amazonaws.com/events/Kahramanmaras-turkey-earthquake-23/ard/37/031131233233/2023-02-08/10300500D9F8D200-visual.tif'
 
-url_post = 'https://maxar-opendata.s3.us-west-2.amazonaws.com/events/Kahramanmaras-turkey-earthquak>
+url_post = 'https://maxar-opendata.s3.us-west-2.amazonaws.com/events/Kahramanmaras-turkey-earthquake-23/ard/37/031131233233/2023-02-08/10300500D9F8D200-visual.tif'
 
 m.split_map(
    left_layer = url_pre, right_layer= url_post, left_label='pre-event', right_label='post-event',
