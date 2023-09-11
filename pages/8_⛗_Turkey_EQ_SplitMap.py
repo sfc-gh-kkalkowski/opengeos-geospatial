f9d884673b96c52053b9985b9e93649747769925
Ramesh De Silva <desilvarami@gmail.com>
2023-09-11 16:06:18 +0530

    Create 8_⛗_Turkey_EQ_SplitMap.py
    New page for Turkey EQ

---
+++ pages/8_⛗_Turkey_EQ_SplitMap.py
@@ -0,0 +1,21 @@
+
+import streamlit as st
+import leafmap.foliumap as leafmap
+import geopandas as gpd
+
+st.set_page_config(layout="wide")
+
+st.title("Turkey Earthquake-Split Map")
+
+m = leafmap.Map()
+
+
+url_pre = 'https://maxar-opendata.s3.us-west-2.amazonaws.com/events/Kahramanmaras-turkey-earthquake>
+
+url_post = 'https://maxar-opendata.s3.us-west-2.amazonaws.com/events/Kahramanmaras-turkey-earthquak>
+
+m.split_map(
+   left_layer = url_pre, right_layer= url_post, left_label='pre-event', right_label='post-event',
+   )
+
+m.set_center(36.9265, 37.5762, 16)
