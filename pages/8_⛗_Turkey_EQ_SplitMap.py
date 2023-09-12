8c3bf1e700afca89443d02b36e1ed736c1b79917
Ramesh De Silva <desilvarami@gmail.com>
2023-09-12 09:51:41 +0530

    Update 8_⛗_Turkey_EQ_SplitMap.py
    import os

--- pages/8_⛗_Turkey_EQ_SplitMap.py
+++ pages/8_⛗_Turkey_EQ_SplitMap.py
@@ -1,19 +1,18 @@
 
 import streamlit as st
 import leafmap.foliumap as leafmap
-import geopandas as gpd
+#import geopandas as gpd
+import os
 
 st.set_page_config(layout="wide")
 
 st.title("Turkey Earthquake-Split Map")
 
-m = leafmap.Map()
-
-
 url_pre = 'https://maxar-opendata.s3.us-west-2.amazonaws.com/events/Morocco-Earthquake-Sept-2023/ard/29/120202012211/2021-05-02/104001006800CE00-visual.tif'
 
 url_post = 'https://maxar-opendata.s3.us-west-2.amazonaws.com/events/Morocco-Earthquake-Sept-2023/ard/29/120202012211/2023-09-10/10300500E4F91700-visual.tif'
 
+m = leafmap.Map()
 m.split_map(
    left_layer = url_pre, right_layer= url_post, left_label='pre-event', right_label='post-event',
    )
