d91ab17def28af915dc7b227bf977e4b159f77db
Ramesh De Silva <desilvarami@gmail.com>
2023-09-18 14:30:15 +0530

    Update 11_🧱_Maxar_Mosaic.py
    add footprints as json

--- pages/11_🧱_Maxar_Mosaic.py
+++ pages/11_🧱_Maxar_Mosaic.py
@@ -9,13 +9,13 @@ st.title("Turkey Earthquake-COG Mosaic")
 m = leafmap.Map()
 
 pre = leafmap.maxar_tile_url('Kahramanmaras-turkey-earthquake-23', '10300100D797E100', dtype='json')
-#pre = 'https://maxar-opendata.s3.amazonaws.com/events/Kahramanmaras-turkey-earthquake-23/ard/acquisition_collections/10300100D797E100_collection.json'
+preurl = 'https://maxar-opendata.s3.amazonaws.com/events/Kahramanmaras-turkey-earthquake-23/ard/acquisition_collections/10300100D797E100_collection.json'
 
 post = leafmap.maxar_tile_url('Kahramanmaras-turkey-earthquake-23', '10300500D9F8D200', dtype='json')
 
 url = 'https://open.gishub.org/maxar-open-data/datasets/Kahramanmaras-turkey-earthquake-23/10300100D797E100.json'
 m.add_stac_layer(url, name="Mosaic")
-
+m.add_geojson(preurl, layer_name="Footprints")
 m.split_map(
     left_layer =pre, right_layer=post, left_label='pre-event', right_label='post-event',
 )
